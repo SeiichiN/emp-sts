2f1d9e8d1718bacf0af1715e0ad73f905e1c4a83
SeiichiN <billie175@gmail.com>
2021-02-19 07:12:33 +0900

    文字を追加

--- src/main/java/com/billies_works/app/controller/IndexController.java
+++ src/main/java/com/billies_works/app/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * クライアントからのリクエストが "/" だったとき
  * "index"ビューを返す
  * @author se-ichi
+ * まず最初にこれを作る
  */
 @Controller
 public class IndexController {
